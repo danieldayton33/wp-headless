e005733b6bb696fdc9f049ed167d073c27d5ec6a
Andrew Gillingham <andrewg@carimus.com>
2021-04-27 14:31:31 -0400

    refactor: fix options page register so it shows in graphql response, add acf-json

--- wp-content/themes/carimus-headless/functions.php
+++ wp-content/themes/carimus-headless/functions.php
@@ -1,6 +1,5 @@
 <?php
 // TODO: Register custom post types
-// TODO: Register ACF fields/settings
 
 add_action('init', function() {
     if( function_exists('acf_add_options_page') ) {
@@ -11,19 +10,7 @@ add_action('init', function() {
             'capability'	=> 'edit_posts',
             'redirect'		=> false,
             'show_in_graphql' => true,
+            'type' => 'options_page',
         ));
-        
-        // TODO: Save for later usage
-        // acf_add_options_sub_page(array(
-        //     'page_title' 	=> 'Theme Header Settings',
-        //     'menu_title'	=> 'Header',
-        //     'parent_slug'	=> 'theme-general-settings',
-        // ));
-        
-        // acf_add_options_sub_page(array(
-        //     'page_title' 	=> 'Theme Footer Settings',
-        //     'menu_title'	=> 'Footer',
-        //     'parent_slug'	=> 'theme-general-settings',
-        // ));
     }
 });
