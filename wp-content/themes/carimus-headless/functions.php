890e0e3c6cd4c9b573f5c9ed41724f9a35a9fc1e
Andrew Gillingham <andrewg@carimus.com>
2021-04-27 09:04:15 -0400

    feat: base ACF options page setup

--- wp-content/themes/carimus-headless/functions.php
+++ wp-content/themes/carimus-headless/functions.php
@@ -1,3 +1,28 @@
 <?php
 // TODO: Register custom post types
 // TODO: Register ACF fields/settings
+
+add_action('init', function() {
+    if( function_exists('acf_add_options_page') ) {
+        acf_add_options_page(array(
+            'page_title' 	=> 'General Settings',
+            'menu_title'	=> 'Theme Settings',
+            'menu_slug' 	=> 'theme-general-settings',
+            'capability'	=> 'manage_options',
+            'redirect'		=> false
+        ));
+        
+        // TODO: Save for later usage
+        // acf_add_options_sub_page(array(
+        //     'page_title' 	=> 'Theme Header Settings',
+        //     'menu_title'	=> 'Header',
+        //     'parent_slug'	=> 'theme-general-settings',
+        // ));
+        
+        // acf_add_options_sub_page(array(
+        //     'page_title' 	=> 'Theme Footer Settings',
+        //     'menu_title'	=> 'Footer',
+        //     'parent_slug'	=> 'theme-general-settings',
+        // ));
+    }
+});
