fbc51ac9fb0e8c76ef83c0fef4cc0ad2f32a7eab
danieldcarimus <60147529+danieldcarimus@users.noreply.github.com>
2021-05-03 17:09:45 -0400

    added fields to acf json

--- wp-content/themes/carimus-headless/inc/custom-fields.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(__DIR__ . '/custom-fields/header-social-links.php');
-require_once(__DIR__ . '/custom-fields/options-misc.php');
-require_once(__DIR__ . '/custom-fields/options-pages.php');
-require_once(__DIR__ . '/custom-fields/page-fields.php');
